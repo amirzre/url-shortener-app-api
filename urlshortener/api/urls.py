2198ae866e678d3d6292a70ed923ad9c5b96c2dc
Amir Zare <amirzare309@yahoo.com>
2024-07-31 04:27:52 -0400

    fix: add env variables and remove unnecessary url path.

--- urlshortener/api/urls.py
+++ urlshortener/api/urls.py
@@ -3,9 +3,4 @@ from django.urls import include, path
 urlpatterns = [
     path("auth/", include(("urlshortener.authentication.urls", "authentication"))),
     path("users/", include(("urlshortener.users.urls", "users"))),
-    path("errors/", include(("urlshortener.errors.urls", "errors"))),
-    path("files/", include(("urlshortener.files.urls", "files"))),
-    path(
-        "google-oauth2/", include(("urlshortener.blog_examples.google_login_server_flow.urls", "google-oauth2"))
-    ),
 ]
