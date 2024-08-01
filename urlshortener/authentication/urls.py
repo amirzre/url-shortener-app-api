5d210370af752dc53c61e9b7e31b06e8c9fb66a2
Amir Zare <amirzare309@yahoo.com>
2024-08-01 11:56:25 -0400

    fix: add authorization with jwt.

--- urlshortener/authentication/urls.py
+++ urlshortener/authentication/urls.py
@@ -1,33 +1,21 @@
 from django.urls import include, path
-
-from .apis import (
-    UserJwtLoginApi,
-    UserJwtLogoutApi,
-    UserMeApi,
-    UserSessionLoginApi,
-    UserSessionLogoutApi,
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
 )
 
+from .apis import UserMeApi
+
 urlpatterns = [
-    path(
-        "session/",
-        include(
-            (
-                [
-                    path("login/", UserSessionLoginApi.as_view(), name="login"),
-                    path("logout/", UserSessionLogoutApi.as_view(), name="logout"),
-                ],
-                "session",
-            )
-        ),
-    ),
     path(
         "jwt/",
         include(
             (
                 [
-                    path("login/", UserJwtLoginApi.as_view(), name="login"),
-                    path("logout/", UserJwtLogoutApi.as_view(), name="logout"),
+                    path("login/", TokenObtainPairView.as_view(), name="login"),
+                    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+                    path("verify/", TokenVerifyView.as_view(), name="verify"),
                 ],
                 "jwt",
             )
