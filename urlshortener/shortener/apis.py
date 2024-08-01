0dab091368c236e3ea765c2a02e55d80963e49f6
Amir Zare <amirzare309@yahoo.com>
2024-08-01 03:02:02 -0400

    feat: add shortener redirect api.

--- urlshortener/shortener/apis.py
+++ urlshortener/shortener/apis.py
@@ -1,16 +1,13 @@
 from django.conf import settings
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from drf_spectacular.utils import extend_schema
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from urlshortener.shortener.models import Shortner
+from urlshortener.shortener.selectors import get_long_url
 from urlshortener.shortener.services import create_or_get_short_url
-
-
-class ShortenerListApi(APIView):
-    pass
 
 
 class ShortenerDetailApi(APIView):
@@ -23,7 +20,7 @@ class ShortenerDetailApi(APIView):
         class Meta:
             model = Shortner
             fields = ("short_url", "full_short_url")
-        
+
         def get_full_short_url(self, obj):
             app_domain = settings.APP_DOMAIN
             return f"{app_domain}/{obj.short_url}"
@@ -39,3 +36,28 @@ class ShortenerDetailApi(APIView):
             return Response({"detail": "error - " + str(ex)}, status=status.HTTP_400_BAD_REQUEST)
         output_serializer = self.OutputShortenerSerializer(shortener)
         return Response(output_serializer.data, status=status.HTTP_201_CREATED if created else status.HTTP_200_OK)
+
+
+class RedirectShortURLView(APIView):
+    class InputRedirectSerializer(serializers.Serializer):
+        short_url = serializers.CharField(required=True, max_length=2048)
+
+    class OutputRedirectSerializer(serializers.Serializer):
+        long_url = serializers.URLField()
+
+    @extend_schema(
+        request=InputRedirectSerializer,
+        responses=OutputRedirectSerializer
+    )
+    def get(self, request, short_url):
+        serializer = self.InputRedirectSerializer(data={"short_url": short_url})
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            long_url = get_long_url(short_url=serializer.validated_data.get("short_url"))
+        except ObjectDoesNotExist:
+            return Response({"detail": "Long URL not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        output_serializer = self.OutputRedirectSerializer(data={"long_url": long_url})
+        output_serializer.is_valid(raise_exception=True)
+        return Response(data=output_serializer.data, status=status.HTTP_200_OK)
--- urlshortener/shortener/urls.py
+++ urlshortener/shortener/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .apis import ShortenerDetailApi, ShortenerListApi
+from .apis import RedirectShortURLView, ShortenerDetailApi
 
 urlpatterns = [
-    path("", ShortenerListApi.as_view(), name="detail"),
-    path("create/", ShortenerDetailApi.as_view(), name="create"),
+    path("create/", ShortenerDetailApi.as_view(), name="create_short_url"),
+    path("<str:short_url>/", RedirectShortURLView.as_view(), name="redirect_short_url"),
 ]
