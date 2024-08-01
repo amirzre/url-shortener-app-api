7f41efb663cc5424c173d75476f149224242291c
Amir Zare <amirzare309@yahoo.com>
2024-08-01 03:22:19 -0400

    fix: add db index for short url field.

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -5,8 +5,8 @@ from urlshortener.common.models import BaseModel
 
 class Shortner(BaseModel):
     snowflake_id = models.BigIntegerField(unique=True)
-    short_url = models.CharField(max_length=50, unique=True)
-    long_url = models.URLField(unique=True, db_index=True)
+    short_url = models.CharField(max_length=50, unique=True, db_index=True)
+    long_url = models.URLField(unique=True)
 
     def __str__(self) -> str:
         return self.long_url
