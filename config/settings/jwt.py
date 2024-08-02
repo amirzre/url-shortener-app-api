ca6a828397b1ad992fdee6e8dee4dc191ee4219d
Amir Zare <amirzare309@yahoo.com>
2024-08-02 09:21:47 -0400

    fix: fix read secret key from env and set default in jwt settings.

--- config/settings/jwt.py
+++ config/settings/jwt.py
@@ -1,6 +1,9 @@
+import os
 from datetime import timedelta
 
-from config.env import env
+from config.env import BASE_DIR, env
+
+env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
@@ -9,7 +12,7 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
     "ALGORITHM": "HS256",
-    "SIGNING_KEY": env("SECRET_KEY"),
+    "SIGNING_KEY": env("SECRET_KEY", default="*6hkpfcg66b*wr+du07m**aez!&%*5uniwf$&g17y24zx^7*2"),
     "AUTH_HEADER_TYPES": ("Bearer",),
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
 }
