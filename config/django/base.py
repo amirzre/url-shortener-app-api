190819a7e422c66706655649c2a391915da33d01
Amir Zare <amirzare309@yahoo.com>
2024-08-02 09:32:48 -0400

    fix: remove rest framework jwt package.

--- config/django/base.py
+++ config/django/base.py
@@ -29,7 +29,7 @@ THIRD_PARTY_APPS = [
     "django_filters",
     "corsheaders",
     "django_extensions",
-    "rest_framework_jwt",
+    "rest_framework_simplejwt",
     "drf_spectacular",
     "drf_spectacular_sidecar",
 ]
